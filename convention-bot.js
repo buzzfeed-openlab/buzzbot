65db9e16c397f5324ca8113eab60f267f4c0c1bf
WestleyArgentum <westleyargentum@gmail.com>
2016-04-30 21:42:03 +0000

    just testing

--- convention-bot.js
+++ convention-bot.js
@@ -1,1 +1,16 @@
 
+var express = require('express');
+var app = express();
+
+app.get('/hook/', function (req, res) {
+  console.log('hook!');
+  if (req.query['hub.verify_token'] === 'beepboopbop') {
+    console.log('success!');
+    res.send(req.query['hub.challenge']);
+  }
+
+  res.send('Error, wrong validation token');
+});
+
+app.listen(443);
+
