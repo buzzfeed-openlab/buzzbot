84da8bec583385ab4415b1877acbeaa3d76885d8
Westley Hennigh-Palermo <westleyargentum@gmail.com>
2016-05-01 17:47:23 -0700

    initial question setup

--- convention-bot.js
+++ convention-bot.js
@@ -14,11 +14,72 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+var users = {};
+
+var initialQuestions = [
+    {
+        "id": "e42d0ffc-cdf2-41f8-a18d-988529d6b86f",
+        "attachment": {
+          "type":"template",
+          "payload":{
+            "template_type":"button",
+            "text":"Why are you interested in the Republican convention?",
+            "buttons":[
+              {
+                "title":"I'll be in Cleveland",
+                "type":"postback",
+                "payload":"e42d0ffc-cdf2-41f8-a18d-988529d6b86f:in-town"
+              },
+              {
+                "title":"I'm a delegate",
+                "type":"postback",
+                "payload":"e42d0ffc-cdf2-41f8-a18d-988529d6b86f:delegate"
+              },
+              {
+                "title":"For another reason",
+                "type":"postback",
+                "payload":"e42d0ffc-cdf2-41f8-a18d-988529d6b86f:other-reason"
+              }
+            ]
+          }
+        }
+    }
+];
+
+function handleIncomingMessage(token, event) {
+    var sender = event.sender.id,
+        text = event.message.text;
+
+    if (!users[sender]) {
+        users[sender] = {
+            id: sender,
+            messages: {}
+        };
+
+        startInitialConversation(token, sender);
+    } else {
+        console.log('WEVE SEEN THIS USER!');
+    }
+}
+
+function handlePostBack(token, event) {
+
+}
+
+function startInitialConversation(token, user) {
+    console.log('starting initial conversation with user:', user);
+
+    for (var i = 0; i < initialQuestions.length; ++i) {
+        var question = initialQuestions[i];
+        sendTemplateMessage(token, user, question);
+    }
+}
 
 function sendTextMessage(token, recipient, text) {
     messageData = {
         text:text
-    }
+    };
+
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: {access_token:token},
@@ -27,6 +88,27 @@ function sendTextMessage(token, recipient, text) {
             recipient: {id:recipient},
             message: messageData,
         }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+}
+
+function sendTemplateMessage(token, recipient, message) {
+    delete message.id;
+
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: { id:recipient },
+            message: message,
+        }
+
     }, function(error, response, body) {
         if (error) {
             console.log('Error sending message: ', error);
@@ -47,17 +129,20 @@ app.get('/hook/', function (req, res) {
 });
 
 app.post('/hook/', function (req, res) {
-    messaging_events = req.body.entry[0].messaging;
-    for (i = 0; i < messaging_events.length; i++) {
-        event = req.body.entry[0].messaging[i];
-        sender = event.sender.id;
-        if (event.message && event.message.text) {
-            text = event.message.text;
+    var messaging_events = req.body.entry[0].messaging,
+        status = 200;
 
-            sendTextMessage(config.pageToken, sender, "Text received, echo: "+ text.substring(0, 200));
+    for (i = 0; i < messaging_events.length; i++) {
+        var event = req.body.entry[0].messaging[i];
+
+        if (event.message && event.message.text) {
+            handleIncomingMessage(config.pageToken, event);
+        } else {
+            console.log('unknown event type: ', event);
         }
     }
-    res.sendStatus(200);
+
+    res.sendStatus(status);
 });
 
 app.listen(8000);
