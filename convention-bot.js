fe1267c2d70656fde4dd2b95aeb1e2cb59187804
Westley Hennigh-Palermo <westleyargentum@gmail.com>
2016-05-25 22:36:22 -0700

    no longer need hardcoded data

--- convention-bot.js
+++ convention-bot.js
@@ -7,6 +7,10 @@ import { User, Controller } from './db';
 
 const config = require('./config.js');
 
+var initialMessages = [
+    1,
+];
+
 // var config = {
 //     pageToken: process.env.pageToken,
 //     verifyToken: process.env.verifyToken,
@@ -35,88 +39,6 @@ app.use(function (error, req, res, next) {
     next();
   }
 });
-
-var users = {};
-
-var messages = {
-    "e42d0ffc-cdf2-41f8-a18d-988529d6b86f": {
-        "id": "e42d0ffc-cdf2-41f8-a18d-988529d6b86f",
-        "attachment": {
-          "type":"template",
-          "payload":{
-            "template_type":"button",
-            "text":"Hi! Nice to meet you.\n\nWhy are you interested in the Republican convention?",
-            "buttons":[
-              {
-                "title":"I'll be in Cleveland",
-                "type":"postback",
-                "payload":"e42d0ffc-cdf2-41f8-a18d-988529d6b86f:in-town"
-              },
-              {
-                "title":"I'm a delegate",
-                "type":"postback",
-                "payload":"e42d0ffc-cdf2-41f8-a18d-988529d6b86f:delegate"
-              },
-              {
-                "title":"For another reason",
-                "type":"postback",
-                "payload":"e42d0ffc-cdf2-41f8-a18d-988529d6b86f:other-reason"
-              }
-            ]
-          }
-        }
-    },
-    "482a57a7-b381-4db5-ab65-0d3e73d789c4": {
-        "id": "482a57a7-b381-4db5-ab65-0d3e73d789c4",
-        "text": "Great, we'll be on the ground as well. We'll push updates your way and reach out with any questions we have."
-    },
-    "677c795a-ea2a-4cd2-8add-180e4cb40d3f": {
-        "id": "677c795a-ea2a-4cd2-8add-180e4cb40d3f",
-        "text": "Sounds good, we'll push updates your way and reach out with any questions we have."
-    },
-    "9ce84a73-311c-453b-b140-aa6f0353025f": {
-        "id": "9ce84a73-311c-453b-b140-aa6f0353025f",
-        "attachment": {
-          "type":"template",
-          "payload":{
-            "template_type":"button",
-            "text":"Oh? Are you:",
-            "buttons":[
-              {
-                "title":"Just interested",
-                "type":"postback",
-                "payload":"9ce84a73-311c-453b-b140-aa6f0353025f:remote"
-              },
-              {
-                "title":"For another reason",
-                "type":"postback",
-                "payload":"9ce84a73-311c-453b-b140-aa6f0353025f:other-reason"
-              }
-            ]
-          }
-        }
-    },
-    "b5017034-8235-4cc5-9b1c-e7b942ff4d13": {
-        "id": "b5017034-8235-4cc5-9b1c-e7b942ff4d13",
-        "text": "Great, we'll push the latest your way"
-    },
-    "6fa03b19-28ae-4c7c-a7c0-e40e62b0478b": {
-        "id": "6fa03b19-28ae-4c7c-a7c0-e40e62b0478b",
-        "text": "Heh, sorry about that, could you tell us why you're interested in your own words?"
-    }
-};
-
-var initialMessages = [
-    1,
-];
-
-var messageTriggers = {
-    "e42d0ffc-cdf2-41f8-a18d-988529d6b86f:in-town": ["482a57a7-b381-4db5-ab65-0d3e73d789c4"],
-    "e42d0ffc-cdf2-41f8-a18d-988529d6b86f:delegate": ["677c795a-ea2a-4cd2-8add-180e4cb40d3f"],
-    "e42d0ffc-cdf2-41f8-a18d-988529d6b86f:other-reason": ["9ce84a73-311c-453b-b140-aa6f0353025f"],
-    "9ce84a73-311c-453b-b140-aa6f0353025f:remote": ["b5017034-8235-4cc5-9b1c-e7b942ff4d13"],
-    "9ce84a73-311c-453b-b140-aa6f0353025f:other-reason": ["6fa03b19-28ae-4c7c-a7c0-e40e62b0478b"]
-};
 
 function parseTag(tag) {
     var tagData = tag.split(':');
