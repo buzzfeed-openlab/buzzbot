84d041959b0637818bfad07352021a556919707e
Westley Hennigh-Palermo <westleyargentum@gmail.com>
2016-05-31 16:05:01 -0700

    basic formatting of message data

--- client/js/Message.js
+++ client/js/Message.js
@@ -12,6 +12,48 @@ export default class Message extends React.Component {
             return <div></div>;
         }
 
-        return <div>message: {JSON.stringify(this.props.message)}</div>;
+        const messageData = JSON.parse(this.props.message.data);
+
+        var messageBodyDiv;
+        if (messageData.attachment && messageData.attachment.payload) {
+            const data = messageData.attachment.payload;
+
+            var buttons;
+            if (data.buttons) {
+                buttons = data.buttons.map((buttonData) => {
+                    return (
+                        <Row>
+                            <Col md={12}>
+                                {buttonData.title}
+                            </Col>
+                        </Row>
+                    );
+                });
+            }
+
+            messageBodyDiv = (
+                <div>
+                <Row>
+                    <Col md={12}>
+                        {data.text}
+                    </Col>
+                </Row>
+                {buttons}
+                </div>
+            );
+        } else {
+            console.log('WARNING unknown message type: ', messageData);
+        }
+
+        return (
+            <div>
+            <Row>
+                <Col md={12}>
+                    Message id: {this.props.message.id}
+                </Col>
+            </Row>
+            {messageBodyDiv}
+            </div>
+        );
     }
 }
