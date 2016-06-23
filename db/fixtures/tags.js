703bd72849139920c0b18d3fe28e84e49e563729
Westley Hennigh-Palermo <westleyargentum@gmail.com>
2016-06-22 18:03:48 -0700

    initial import of scripted intro questions

--- db/fixtures/tags.js
+++ db/fixtures/tags.js
@@ -3,7 +3,7 @@ module.exports = [
         model: 'Tag',
         data: {
             id: 1,
-            tag: 'in-town',
+            tag: 'at-convention',
             messageId: 1
         }
     },
@@ -11,32 +11,160 @@ module.exports = [
         model: 'Tag',
         data: {
             id: 2,
-            tag: 'delegate',
+            tag: 'not-at-convention',
             messageId: 1
         }
     },
+
     {
         model: 'Tag',
         data: {
             id: 3,
-            tag: 'other-reason',
-            messageId: 1
+            tag: 'protesting',
+            messageId: 2
         }
     },
     {
         model: 'Tag',
         data: {
             id: 4,
-            tag: 'remote',
-            messageId: 4
+            tag: 'attending',
+            messageId: 2
         }
     },
     {
         model: 'Tag',
         data: {
             id: 5,
-            tag: 'other-reason',
+            tag: 'reporting',
+            messageId: 2
+        }
+    },
+
+    {
+        model: 'Tag',
+        data: {
+            id: 6,
+            tag: 'can-trump-be-ousted?',
+            messageId: 3
+        }
+    },
+    {
+        model: 'Tag',
+        data: {
+            id: 7,
+            tag: 'whats-the-schedule?',
+            messageId: 3
+        }
+    },
+    {
+        model: 'Tag',
+        data: {
+            id: 8,
+            tag: 'are-there-arrests?',
+            messageId: 3
+        }
+    },
+
+    {
+        model: 'Tag',
+        data: {
+            id: 9,
+            tag: 'protester-might-send-photos',
             messageId: 4
+        }
+    },
+    {
+        model: 'Tag',
+        data: {
+            id: 10,
+            tag: 'protester-will-not-send-photos',
+            messageId: 4
+        }
+    },
+
+    {
+        model: 'Tag',
+        data: {
+            id: 11,
+            tag: 'attendee-might-send-photos',
+            messageId: 5
+        }
+    },
+    {
+        model: 'Tag',
+        data: {
+            id: 12,
+            tag: 'attendee-will-not-send-photos',
+            messageId: 5
+        }
+    },
+
+    {
+        model: 'Tag',
+        data: {
+            id: 13,
+            tag: 'reporter-has-been-denied-access',
+            messageId: 6
+        }
+    },
+    {
+        model: 'Tag',
+        data: {
+            id: 14,
+            tag: 'reporter-has-not-been-denied-access',
+            messageId: 6
+        }
+    },
+
+    {
+        model: 'Tag',
+        data: {
+            id: 15,
+            tag: 'protester-confirmed-will-send-photos',
+            messageId: 10
+        }
+    },
+    {
+        model: 'Tag',
+        data: {
+            id: 16,
+            tag: 'protester-will-not-send-photos',
+            messageId: 10
+        }
+    },
+
+    {
+        model: 'Tag',
+        data: {
+            id: 17,
+            tag: 'attendee-confirmed-will-send-photos',
+            messageId: 13
+        }
+    },
+    {
+        model: 'Tag',
+        data: {
+            id: 18,
+            tag: 'attendee-will-not-send-photos',
+            messageId: 13
+        }
+    },
+
+    {
+        model: 'Tag',
+        data: {
+            id: 19,
+            tag: 'journalist-will-share-story',
+            messageId: 15
+        }
+    },
+    {
+        model: 'Tag',
+        data: {
+            id: 20,
+            tag: 'journalist-will-not-share-story',
+            messageId: 15
         }
     },
 ]
