04b74a836a1cc4aec3b88808ed8392068ee91bb4
Srivatsa Ray <hi@sri.so>
2016-07-14 19:42:16 -0700

    adding sequelize_config

---
+++ db/sequelize_config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  development: {
+    username: process.env.PGUSER || 'conventionbot',
+    password: process.env.PGPASSWORD || null,
+    database: process.env.PGDB || 'convention_bot_development',
+    host: process.env.PGHOST || '127.0.0.1',
+    dialect: 'postgres'
+  },
+  test: {
+    username: process.env.PGUSER || 'conventionbot',
+    password: process.env.PGPASSWORD || null,
+    database: process.env.PGDB || 'convention_bot_test',
+    host: process.env.PGHOST || '127.0.0.1',
+    dialect: 'postgres'
+  },
+  production: {
+    use_env_variable: 'POSTGRES_DB_URL',
+    username: process.env.PGUSER || 'conventionbot',
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDB || 'convention_bot_production',
+    host: process.env.PGHOST,
+    dialect: 'postgres'
+  }
+};
